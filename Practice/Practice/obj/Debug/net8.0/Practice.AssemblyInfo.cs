6e374728886a505fd47e9860ec75a4bffa12e395
jeeeybiii <macuj1@student.op.ac.nz>
2026-09-24 15:00:05 +1200

    merging code

--- Practice/Practice/obj/Debug/net8.0/Practice.AssemblyInfo.cs
+++ Practice/Practice/obj/Debug/net8.0/Practice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47f7c5c5d1beadc7731bec76ab840b56884d6cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36ca795aee4dd555d11681cab165f63deddd829")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
